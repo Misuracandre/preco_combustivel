3658050fd533c02a7624b7616e2dc6297ae84ac9
André misuraca <m1sur4k@gmail.com>
2023-03-05 21:40:39 -0300

    Adicionar arquivos de projeto.

---
+++ preco_combustivel/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        double alcool, gasolina;
+
+        Console.WriteLine("Informe preço do alcool");
+        alcool = double.Parse(Console.ReadLine());
+
+        Console.WriteLine("Informe o preço da gasolina");
+        gasolina = double.Parse(Console.ReadLine());
+
+        if (alcool / gasolina < 0.7)
+        {
+            Console.WriteLine("O álcool está mais vantajoso.");
+        }
+
+        else
+        {
+            Console.WriteLine("A gasolina está mais vantajosa.");
+        }
+    }
+}
